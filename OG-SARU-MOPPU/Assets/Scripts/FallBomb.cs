0f7c0a3d8f0ea98a568706e1163f767965f351f8
DA\nwuser <noppo.kappa@gmail.com>
2018-11-07 04:15:25 +0900

    【ねむい】爆発のアニメーション・制限時間・シーン遷移・音

--- OG-SARU-MOPPU/Assets/Scripts/FallBomb.cs
+++ OG-SARU-MOPPU/Assets/Scripts/FallBomb.cs
@@ -13,6 +13,9 @@ public class FallBomb : MonoBehaviour
     //爆弾の着地フラグ
     private bool Landing;
 
+    //陣地破壊
+    private bool bombGround;
+
     //ランダム変数
     private int randNum;
 
@@ -22,8 +25,11 @@ public class FallBomb : MonoBehaviour
     [SerializeField]
     private GameObject landManagerObj;
 
+    private Animator m_animator;
+
     private void Start()
     {
+        m_animator = GetComponent<Animator>();
         m_rigidbody2D = GetComponent<Rigidbody2D>();
 
         //生成時に0～3のランダム数値格納
@@ -42,8 +48,7 @@ public class FallBomb : MonoBehaviour
 
     private void BombMove()
     {
-        if (!Landing)
-        {
+        if (!Landing){
             transform.position = Vector3.Lerp(transform.position, targetObject.transform.position, 0.05f);
         }
     }
@@ -57,7 +62,7 @@ public class FallBomb : MonoBehaviour
         if (transform.position.y > Camera.main.transform.position.y + 10)
         {
             Destroy(this.gameObject);
-        }
+        }   
 
     }
 
@@ -65,6 +70,24 @@ public class FallBomb : MonoBehaviour
     {
         Landing = true; Debug.Log("着地");
         m_rigidbody2D.gravityScale = 1f;
+        StartCoroutine("Bomber");
     }
 
+
+    private void OnCollisionStay2D(Collision2D collision){
+        if (bombGround)
+        {
+            if (collision.gameObject.tag == "Ground"){
+                Destroy(collision.gameObject);
+            }
+            Destroy(gameObject);
+        }
+    }
+
+    IEnumerator Bomber(){
+        yield return new WaitForSeconds(2f);
+        m_animator.Play("BomBom");
+        yield return new WaitForSeconds(4f);
+        bombGround = true;
+    }
 }
---
+++ OG-SARU-MOPPU/Assets/Scripts/RimitTime.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class RimitTime : MonoBehaviour {
+
+    //制限時間
+    private float time = 60;
+
+    private Text m_text;
+	// Use this for initialization
+	void Start () {
+        m_text = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        time -= Time.deltaTime;
+        m_text.text = "制限時間:"+Mathf.Round(time);
+
+        //ゲーム終了処理
+        if (time <= 0)
+        {
+            m_text.text = "終了";
+            //SceneManager.LoadScene("");
+        }
+	}
+}
