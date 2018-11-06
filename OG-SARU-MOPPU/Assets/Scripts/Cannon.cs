515aff529eb635e1142862c11a6d70a09e9e62d0
DA\nwuser <noppo.kappa@gmail.com>
2018-11-06 22:31:15 +0900

    [いまできてるところだよ]

---
+++ OG-SARU-MOPPU/Assets/Scripts/Cannon.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannon : MonoBehaviour {
+
+    private GameObject col_bomb;
+    private Rigidbody2D bombRigidbody;
+
+    private bool Shooting;
+
+    private void OnTriggerEnter2D(Collider2D arg_col){
+        if (!Shooting)
+        {
+            if (arg_col.tag == "bomb")
+            {
+                col_bomb = arg_col.gameObject;
+                bombRigidbody = col_bomb.GetComponent<Rigidbody2D>();
+                col_bomb.transform.position = transform.position;
+                col_bomb.SetActive(false);
+                Shooting = true;
+                StartCoroutine("bomb");
+            }
+        }
+    }
+
+    IEnumerator bomb(){
+        yield return new WaitForSeconds(1f);
+        col_bomb.SetActive(true);
+        bombRigidbody.AddForce(new Vector3(0,50f,0),ForceMode2D.Impulse);
+    }
+}
--- OG-SARU-MOPPU/Assets/Scripts/Player.cs
+++ OG-SARU-MOPPU/Assets/Scripts/Player.cs
@@ -10,8 +10,11 @@ public class Player : MonoBehaviour {
  
     private Rigidbody2D m_rigidbody2D;
 
+    private SpriteRenderer m_spriteRenderer;
+
     private void Start(){
         m_rigidbody2D = GetComponent<Rigidbody2D>();
+        m_spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -20,19 +23,43 @@ public class Player : MonoBehaviour {
 	}
 
     private void PlayerAction(){
-        if (Input.GetKeyDown(KeyCode.W)){
-            if (!m_jumping){
+        if (!m_jumping){
+            if (Input.GetKeyDown(KeyCode.W)){
                 m_rigidbody2D.AddForce(transform.up * 1000f);
-                m_jumping = true;
+               m_jumping = true;
             }
         }
         if (Input.GetKey(KeyCode.D))
-            transform.position += new Vector3(m_moveSpeed, 0, 0);     
+        {
+            transform.position += new Vector3(m_moveSpeed, 0, 0);
+            m_spriteRenderer.flipX = true;
+        }
         if (Input.GetKey(KeyCode.A))
+        {
             transform.position += new Vector3(-m_moveSpeed, 0, 0);
-
+            m_spriteRenderer.flipX = false;
+        }
         if (m_rigidbody2D.velocity.y == 0)
             m_jumping = false;
+    }
+
+    private void OnCollisionStay2D(Collision2D arg_col){
+        if (arg_col.collider.tag == "bomb")
+        {
+            {
+                if (Input.GetKey(KeyCode.Space))
+                {
+                    //左向き
+                    if (!m_spriteRenderer.flipX)
+                        arg_col.gameObject.transform.position += new Vector3(-0.05f, 0, 0);
+
+                    //右向き
+                    if (m_spriteRenderer.flipX)
+                        arg_col.gameObject.transform.position += new Vector3(0.05f, 0, 0);
+                    Debug.Log("ぐり");
+                }
+            }
+        }
     }
 
 }
