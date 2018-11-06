5c96b16a50d23186fcb097f7eaa5e8c6e384fdee
DA\nwuser <noppo.kappa@gmail.com>
2018-11-07 00:31:09 +0900

    [おじ]4つのどこかにいどうする

--- OG-SARU-MOPPU/Assets/Scripts/Cannon.cs
+++ OG-SARU-MOPPU/Assets/Scripts/Cannon.cs
@@ -8,22 +8,37 @@ public class Cannon : MonoBehaviour {
     private Rigidbody2D bombRigidbody;
 
     private bool Shooting;
-
+    
     private void OnTriggerEnter2D(Collider2D arg_col){
+
+        //射出待機時
         if (!Shooting)
         {
+            //ボムと衝突時
             if (arg_col.tag == "bomb")
             {
                 col_bomb = arg_col.gameObject;
                 bombRigidbody = col_bomb.GetComponent<Rigidbody2D>();
+
+                //ボムが大砲の位置まで到達したら
                 col_bomb.transform.position = transform.position;
+                //ボムは一瞬消える
                 col_bomb.SetActive(false);
+
+                //射出状態に移行
                 Shooting = true;
                 StartCoroutine("bomb");
             }
         }
+
+        else{
+            //爆弾が入ったら再び射出待機
+            if (col_bomb) { Shooting = false; }
+        }
     }
 
+
+    //衝突してから一秒後に発射
     IEnumerator bomb(){
         yield return new WaitForSeconds(1f);
         col_bomb.SetActive(true);
---
+++ OG-SARU-MOPPU/Assets/Scripts/bomb.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bomb : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject targetObject;
+
+    private Rigidbody2D m_rigidbody2D;
+
+    //爆弾の着地フラグ
+    private bool Landing;
+
+    //ランダム変数
+    private int randNum;
+
+    [SerializeField]
+    private LandManager m_landManager;
+
+    [SerializeField]
+    private GameObject landManagerObj;
+
+    private void Start(){
+        m_rigidbody2D = GetComponent<Rigidbody2D>();
+
+        //生成時に0～3のランダム数値格納
+        randNum = Random.Range(0, 4);
+
+        //HierarchyからLandManagerのオブジェクト格納
+        landManagerObj = GameObject.Find("LandManager");
+        m_landManager = landManagerObj.GetComponent<LandManager>();
+
+        //生成時にランダムに位置取得 
+        targetObject = m_landManager.landPosition[randNum];
+
+        Debug.Log(randNum);
+
+    }
+
+    private void BombMove(){
+        if (!Landing){
+            transform.position = Vector3.Lerp(transform.position, targetObject.transform.position, 0.05f);
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+        BombMove();
+
+        //上に飛び出たらオブジェクト破壊
+        if (transform.position.y > Camera.main.transform.position.y + 5)
+        {
+            Destroy(this.gameObject);
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision){
+        Landing = true; Debug.Log("着地");
+        m_rigidbody2D.gravityScale = 1f;
+    }
+
+}
